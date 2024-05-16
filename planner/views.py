6752a52c941d47ab06c0bf26e28f774b463fade7
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-17 01:50:47 +0300

    Add register view to views.py
    View for registration page

--- planner/views.py
+++ planner/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import login
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from planner.forms import RegisterForm
+
 
 def index(request):
     return render(request, "planner/index.html")
 
+
+def register(request) -> HttpResponse:
+    if request.method == "GET":
+        form = RegisterForm()
+        return render(request, "registration/register.html", {"form": form})
+
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, "You have singed up successfully.")
+            login(request, user)
+            return redirect("planner:index")
+        else:
+            return render(request, "registration/register.html", {"form": form})

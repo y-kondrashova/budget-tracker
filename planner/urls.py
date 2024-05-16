4349a3581dc91a1a409ef42cc227c5e8e7cce3c8
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-17 01:52:38 +0300

    Add register path to urls.py
    Path for registration page in planner app

--- planner/urls.py
+++ planner/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from planner.views import index
+from planner.views import index, register
 
 urlpatterns = [
     path("", index, name="index"),
+    path("register/", register, name="register")
 ]
 
 app_name = "planner"

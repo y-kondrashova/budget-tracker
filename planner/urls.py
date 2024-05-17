429deded8fee57049eece1cb1288533ea9ced4e6
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-17 12:02:16 +0300

    Add path to category-list in urls.py

--- planner/urls.py
+++ planner/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from planner.views import index, register
+from planner.views import index, register,CategoryListView
 
 urlpatterns = [
     path("", index, name="index"),
-    path("register/", register, name="register")
+    path("register/", register, name="register"),
+    path("categories/", CategoryListView.as_view(), name="category-list")
 ]
 
 app_name = "planner"

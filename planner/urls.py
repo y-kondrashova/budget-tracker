14ff60ab7eef22225c2308519c892b39d6973894
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-17 17:58:22 +0300

    Add path to category-create in urls.py

--- planner/urls.py
+++ planner/urls.py
@@ -1,24 +1,28 @@
 from django.urls import path
 
-from planner.views import index, register,CategoryListView
 from planner.views import (
     index,
     register,
     BudgetListView,
     CategoryListView,
+    CategoryCreateView,
     TransactionListView
 )
 
 urlpatterns = [
     path("", index, name="index"),
     path("register/", register, name="register"),
-    path("categories/", CategoryListView.as_view(), name="category-list")
     path("categories/", CategoryListView.as_view(), name="category-list"),
     path("budgets/", BudgetListView.as_view(), name="budget-list"),
     path(
         "transactions/",
         TransactionListView.as_view(),
         name="transaction-list"
+    ),
+    path(
+        "categories/create/",
+        CategoryCreateView.as_view(),
+        name="category-create"
     ),
 ]
 

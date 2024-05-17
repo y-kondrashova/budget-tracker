bb1a045885688d4b885547bcb2846fa8480ae61d
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-17 15:53:15 +0300

    Add path to budget-list in urls.py

--- planner/urls.py
+++ planner/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
 
 from planner.views import index, register,CategoryListView
+from planner.views import (
+    index,
+    register,
+    BudgetListView,
+    CategoryListView,
+)
 
 urlpatterns = [
     path("", index, name="index"),
     path("register/", register, name="register"),
     path("categories/", CategoryListView.as_view(), name="category-list")
+    path("categories/", CategoryListView.as_view(), name="category-list"),
+    path("budgets/", BudgetListView.as_view(), name="budget-list"),
 ]
 
 app_name = "planner"

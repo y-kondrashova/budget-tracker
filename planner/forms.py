9aa393909b8814a60a1b3da09d036f46fcbb3e4d
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-17 17:49:53 +0300

    Add CategoryForm to forms.py
    Select fields for Category form
    Just owners can see budget fields

--- planner/forms.py
+++ planner/forms.py
@@ -1,8 +1,22 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django import forms
+
+from planner.models import Category, Budget
 
 
 class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ["title", "budget"]
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop("user")
+        super().__init__(*args, **kwargs)
+        self.fields["budget"].queryset = Budget.objects.filter(owner=user)

1a9347ea8abfd56ae0f7c34916579279c7bf4875
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-19 17:04:17 +0300

    Add TransferForm to forms.py in planner app

--- planner/forms.py
+++ planner/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-from planner.models import Budget, Transaction
+from planner.models import Budget, Transaction, Transfer
 
 
 class RegisterForm(UserCreationForm):
@@ -20,3 +20,15 @@ class TransactionForm(forms.ModelForm):
         user = kwargs.pop("user")
         super().__init__(*args, **kwargs)
         self.fields["budget"].queryset = Budget.objects.filter(owner=user)
+
+
+class TransferForm(forms.ModelForm):
+    class Meta:
+        model = Transfer
+        fields = ["from_budget", "to_budget", "amount", "date"]
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop("user")
+        super().__init__(*args, **kwargs)
+        self.fields["from_budget"].queryset = Budget.objects.filter(owner=user)
+        self.fields["to_budget"].queryset = Budget.objects.filter(owner=user)

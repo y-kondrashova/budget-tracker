e9e72649c577029aec886f31795a2c55d49d6d5b
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-20 13:46:02 +0300

    Add DateSearchForm to forms.py in planner app

--- planner/forms.py
+++ planner/forms.py
@@ -59,3 +59,8 @@ class TransferForm(forms.ModelForm):
                 )
 
         return cleaned_data
+
+
+class DateSearchForm(forms.Form):
+    start_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), required=False)
+    end_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), required=False)

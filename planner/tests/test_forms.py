c4b361162bf5a18aa1c8c1e9f8b49ac8a9d10eaf
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-21 11:30:40 +0300

    Add test to SearchFormTest
    Add test_date_search_form_empty that:
    check if created data match,
    check if form is valid when empty

--- planner/tests/test_forms.py
+++ planner/tests/test_forms.py
@@ -15,3 +15,13 @@ class SearchFormTest(TestCase):
         self.assertTrue(form.is_valid())
         self.assertEqual(form.cleaned_data["start_date"], date(2024, 5, 10))
         self.assertEqual(form.cleaned_data["end_date"], date(2024, 5, 15))
+
+    def test_date_search_form_empty(self):
+        form_data = {
+            "start_date": None,
+            "end_date": None
+        }
+        form = DateSearchForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data["start_date"], None)
+        self.assertEqual(form.cleaned_data["end_date"], None)

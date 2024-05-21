f7a1f03e499d555edcc8df283e23efeb993fb364
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-21 11:23:23 +0300

    Add SearchFormTest to tests
    Add test_date_search_form_valid that:
    check if created data match,
    check if form is valid

---
+++ planner/tests/test_forms.py
@@ -0,0 +1,17 @@
+from datetime import date
+
+from django.test import TestCase
+
+from planner.forms import DateSearchForm
+
+
+class SearchFormTest(TestCase):
+    def test_date_search_form_valid(self):
+        form_data = {
+            "start_date": date(2024, 5, 10),
+            "end_date": date(2024, 5, 15)
+        }
+        form = DateSearchForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data["start_date"], date(2024, 5, 10))
+        self.assertEqual(form.cleaned_data["end_date"], date(2024, 5, 15))

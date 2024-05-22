5115d29a6cdcb796ac8be4d934b491986ce945a6
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-22 09:25:56 +0300

    Fixup settings.py
    Add ALLOWED_HOSTS

--- budget_tracker/settings.py
+++ budget_tracker/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["budget-tracker"]
 
 
 # Application definition

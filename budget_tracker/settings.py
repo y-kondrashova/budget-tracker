5a04f7e3a008c0d99b5b6bb04e94bdf25c31dad5
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-22 17:45:45 +0300

    Fixup settings.py
    put back statics

--- budget_tracker/settings.py
+++ budget_tracker/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "django.contrib.staticfiles",
     "djmoney",
     "crispy_forms",
     "crispy_bootstrap4",

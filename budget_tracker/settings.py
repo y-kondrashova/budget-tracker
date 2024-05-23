1bd0de73fb266c994bdf771097e2276d114b7a22
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-23 13:53:25 +0300

    Fixup settings.py
    Add RENDER_EXTERNAL_HOSTNAME

--- budget_tracker/settings.py
+++ budget_tracker/settings.py
@@ -39,6 +39,10 @@ DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = []
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
